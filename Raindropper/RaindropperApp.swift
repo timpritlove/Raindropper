7390d14eb526190a411216a184bba132fabca505
Tim Pritlove <tim@pritlove.org>
2024-12-07 09:59:55 +0100

    Initial Commit

---
+++ Raindropper/RaindropperApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaindropperApp.swift
+//  Raindropper
+//
+//  Created by Tim Pritlove on 07.12.24.
+//
+
+import SwiftUI
+
+@main
+struct RaindropperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RaindropperTests/RaindropperTests.swift
@@ -0,0 +1,17 @@
+//
+//  RaindropperTests.swift
+//  RaindropperTests
+//
+//  Created by Tim Pritlove on 07.12.24.
+//
+
+import Testing
+@testable import Raindropper
+
+struct RaindropperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
